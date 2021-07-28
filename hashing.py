d3721a50affa02c9295d3c82f4dbd86ca54b5cc3
Leonardo Gomes <36674743+LeonardoGomesG@users.noreply.github.com>
2021-07-28 10:48:59 -0300

    Add files via upload

---
+++ hashing.py
@@ -0,0 +1,62 @@
+# Importing libraries
+import time
+import hashlib
+from urllib.request import urlopen, Request
+
+# setting the URL you want to monitor
+url = Request('https://leetcode.com/',
+              headers={'User-Agent': 'Mozilla/5.0'})
+
+
+def get_hashes(urls):
+    return
+
+
+def compare_hashes(url):
+    # to perform a GET request and load the
+    # content of the website and store it in a var
+    response = urlopen(url).read()
+
+    # to create the initial hash
+    currentHash = hashlib.md5(response).hexdigest()
+    print("running")
+    time.sleep(10)
+    while True:
+        try:
+            # perform the get request and store it in a var
+            response = urlopen(url).read()
+
+            # create a hash
+            currentHash = hashlib.md5(response).hexdigest()
+
+            # wait for 30 seconds
+            time.sleep(30)
+
+            # perform the get request
+            response = urlopen(url).read()
+
+            # create a new hash
+            newHash = hashlib.md5(response).hexdigest()
+
+            # check if new hash is same as the previous hash
+            if newHash == currentHash:
+                continue
+
+            # if something changed in the hashes
+            else:
+                # notify
+                print("something changed")
+
+                # again read the website
+                response = urlopen(url).read()
+
+                # create a hash
+                currentHash = hashlib.md5(response).hexdigest()
+
+                # wait for 30 seconds
+                time.sleep(30)
+                continue
+
+        # To handle exceptions
+        except Exception as e:
+            print("error")
